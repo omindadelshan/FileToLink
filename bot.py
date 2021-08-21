d51775ea0e2009a67a59f84492dacef0f7e036ef
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-21 13:53:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ def short(url):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Hello bro😁🤩! I am a File To Link Bot created by @SDBOTsZ.Send me any file (Video, Audio, Photo, Document)📥😇')
+    bot.reply_to(message, '👋Hello I Am Simple File To Link Bot👨‍💻..I Can Genarate Links On Your Files📂.😋Send Me To Media or File I Am Genarate Links In It File..              ❤️This Bot Made By @omindas ❤️..')
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
